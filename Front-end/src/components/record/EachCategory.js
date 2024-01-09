10880ac14415d9b2c665606cdb91d01aa59a4a0e
User <user@Arvinbuyans-MacBook-Pro.local>
2024-01-09 08:38:26 +0800

    jJH

--- Front-end/src/components/record/EachCategory.js
+++ Front-end/src/components/record/EachCategory.js
@@ -1,20 +1,17 @@
 import { useAuth } from "@/app/layout";
 
 export const EachCategory = (props) => {
-  const { filter, setFilter,setRefresh,refresh } = useAuth();
+  const { filter, setFilter, setRefresh, refresh } = useAuth();
 
   return (
     <div className="w-full h-fit flex item-center justify-between">
       <div className="flex gap-2">
         <img
-        src={
-          filter.includes(props.title) ? "hide1.png" : "eye.png"
-        }
-
+          src={filter.includes(props.title) ? "hide1.png" : "eye.png"}
           className="w-5 h-5 mt-[2px]"
           onClick={() => {
-            if(filter.includes(props.category)){
-              setFilter(filter.filter((item)=>item!==props.category));
+            if (filter.includes(props.category)) {
+              setFilter(filter.filter((item) => item !== props.category));
             } else {
               filter.push(props.title);
               setFilter(filter);
@@ -23,7 +20,7 @@ export const EachCategory = (props) => {
             console.log(filter);
           }}
         />
-        
+
         <p>{props.title}</p>
       </div>
       <img className="w-5 h-5" src="let.png"></img>
